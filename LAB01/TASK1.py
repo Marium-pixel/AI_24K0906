8e641b744a7dbd339a8b29b53cbd6325d7618b04
Marium-pixel <imranmarium742@gmail.com>
2026-01-25 17:21:23 +0500

    Create TASK1.py

---
+++ LAB01/TASK1.py
@@ -0,0 +1,11 @@
+#q.1
+name = input("enter your name")
+print (name)
+marks=int(input("enter your marks"))
+if marks >= 85:
+  print("Grade A")
+elif marks >=70:
+  print("Grade B")
+elif marks >= 50:
+  print("Grade C")
+else: print("Fail")
